7c430ba725aa497537cd2740f7cb28fccdfe9856
jamesp <phillips.james@gmail.com>
2019-08-17 18:04:30 -0500

    no need for multiple ngw in a test vpc

--- testvpc/vpc.tf
+++ testvpc/vpc.tf
@@ -17,8 +17,8 @@ module "vpc" {
   enable_dns_hostnames   = true
   enable_dns_support     = true
   enable_nat_gateway     = true
-  single_nat_gateway     = false
-  one_nat_gateway_per_az = true
+  single_nat_gateway     = true
+  one_nat_gateway_per_az = false
 
   ## create vpc endpoints
   enable_s3_endpoint = true
